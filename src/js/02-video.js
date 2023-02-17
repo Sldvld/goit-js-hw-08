241b01541be6733965790a4e8a049c7786a3512c
Sldvlld <sladvlad@gmail.com>
2023-02-17 23:15:42 +0200

    work with player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+    player.on('timeupdate', function(data) {{
+            duration: 61.857
+            percent: 0.049
+            seconds: 3.034
+    }
+        console.log('data', data);
+    });
+
+    player.setCurrentTime(30).then(function(seconds) {
+    }).catch(function(error) {
+            switch (error.name) {
+            case 'RangeError':
+            break;
+            default:
+            break;
+        }
+    });

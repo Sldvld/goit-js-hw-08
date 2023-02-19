6596a1b628aa8cfa250c43214faae626a5d3417b
Sldvlld <sladvlad@gmail.com>
2023-02-19 20:36:50 +0200

    complete 3-rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('form'),
+    textarea: document.querySelector('.feedback-form textarea')
+}
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+const parseData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', evt => {
+    formData[evt.target.name] = evt.target.value;
+});
+
+populateTextarea();
+
+function onTextareaInput(evt){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt){
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+
+function populateTextarea(){
+    if(parseData){
+        const formKeys = Object.keys(parseData);
+        formKeys.map(key => {
+            refs.form.elements[key].value = parseData[key]
+        })
+    }}

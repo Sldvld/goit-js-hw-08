3afac9ecffb722fc3bdfedd2620297a4d1b9ef4a
Sldvld <113293839+Sldvld@users.noreply.github.com>
2023-02-20 16:27:56 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,23 +3,14 @@ import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const KEY_CURRENT_TIME = "videoplayer-current-time";
+
 const currentTime = function(data) {
-    localStorage.setItem("videoplayer-current-time",
+    localStorage.setItem(KEY_CURRENT_TIME,
     JSON.stringify(data.seconds)
     );
-    let time = Number(localStorage.getItem("videoplayer-current-time"));
+    let time = Number(localStorage.getItem(KEY_CURRENT_TIME));
     console.log(time);
 }
-
     player.on("timeupdate", throttle(currentTime, 1000));
-
-    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
-        .then(function(seconds) {
-    })  .catch(function(error) {
-            switch (error.name) {
-            case 'RangeError':
-            break;
-            default:
-            break;
-        }
-    });
+    player.setCurrentTime(localStorage.getItem(KEY_CURRENT_TIME));
